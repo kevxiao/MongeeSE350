233d06c5bd4d95c0ae91c23196e086945453e9f0
unknown <gfmeeuss@ECE-RTOS9.NEXUS.UWATERLOO.CA>
2016-03-03 17:37:01 -0500

    Made the wall clock user function and removed stuff that wasnt supposed to be in the other files

--- P2/src/k_process.c
+++ P2/src/k_process.c
@@ -44,8 +44,6 @@ PCB* gp_priority_end[NUM_PRIORITIES] = {NULL, NULL, NULL, NULL, NULL};
 PCB* gp_priority_begin[NUM_PRIORITIES] = {NULL, NULL, NULL, NULL, NULL};
 
 extern uint32_t g_timer_count;
-extern int g_clock_time;
-extern int g_display_clock;
 
 /**
  * @brief: the null process
@@ -74,10 +72,6 @@ void kcdproc(void) {
 	MSG_BUF* command_msg;
 	//MSG_BUF* string_msg;
 	int sender_id;
-	int numHours;
-	int numMinutes;
-	int numSeconds;
-	char ASCII0 = '0';
 	char* command_text;
 	while (1) {
 		command_msg = (MSG_BUF*) k_receive_message(&sender_id);
@@ -85,34 +79,8 @@ void kcdproc(void) {
 		command_text = command_msg->mtext;
 		//check if valid command
 		if ('%' == command_text[0]) {
-			if ('W' == command_text[1] && 'R' == command_text[2]) {
-				g_clock_time = 0;
-				g_display_clock = 1;
-			}
-			else if ('W' == command_text[1] && 'S' == command_text[2] && ' ' == command_text[3]) {
-				if (':' == command_text[6] && ':' == command_text[9]) {
-					numHours = (int)(command_text[4] - ASCII0) * 10 + (int)(command_text[5] - ASCII0) ;
-					numMinutes = (int)(command_text[7] - ASCII0) * 10 + (int)(command_text[8] - ASCII0);
-					numSeconds = (int)(command_text[10] - ASCII0) * 10 + (int)(command_text[11] - ASCII0);
-					
-					if ( (int)(command_text[4] - ASCII0) < 0 || (int)(command_text[4] - ASCII0) > 9 || (int)(command_text[5] - ASCII0) < 0 || (int)(command_text[5] - ASCII0) > 9
-						|| (int)(command_text[7] - ASCII0) < 0 || (int)(command_text[7] - ASCII0) > 9 || (int)(command_text[8] - ASCII0) < 0 || (int)(command_text[8] - ASCII0) > 9
-					  || (int)(command_text[10] - ASCII0) < 0 || (int)(command_text[10] - ASCII0) > 9 || (int)(command_text[11] - ASCII0) < 0 || (int)(command_text[11] - ASCII0) > 9
-						|| numHours > 23 || numMinutes > 59 || numSeconds > 59) {
-							//Invalid time format error
-						}
-						else {
-							g_clock_time = numSeconds+60*(numMinutes+60*numHours);
-							g_display_clock = 1;
-						}
-							
-				}
-				else {
-					//argument syntax error
-				}
-			}
-			else if ('W' == command_text[1] && 'T' == command_text[2]) {
-				g_display_clock = 0;
+			if ('W' == command_text[1]) {
+				k_send_message(PID_CLOCK, command_msg);
 			}
 			else {
 				//invalid command error
@@ -466,4 +434,21 @@ int k_delayed_send(int process_id, void *message_envelope, int delay) {
 	}
 	
 	return RTX_OK;
+}
+
+void queue_debug_statement(PROC_STATE_E state) {
+	int i = 0;
+	PCB* cur_pcb = NULL;
+	for ( i = 0; i < NUM_PRIORITIES; ++i) {
+		printf("Priority: %d\n\r", i);
+		cur_pcb = gp_priority_begin[i];
+		while (cur_pcb != NULL) {
+			if (cur_pcb->m_state == state /*|| (state == RDY && cur_pcb->m_state == NEW)*/) {
+				#ifdef DEBUG_0
+				printf("\t%u\n\r", cur_pcb->m_pid );
+				#endif
+			}
+			cur_pcb = cur_pcb->mp_next;
+		}
+	}
 }
